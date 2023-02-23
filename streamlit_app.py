6c5d9b958f8b5f3aeb745bacacd690a98c362ba6
潘振兴 <53480775+Pan-zhenxing@users.noreply.github.com>
2023-02-23 22:50:39 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,44 +1,72 @@
 import streamlit as st
-import speech_recognition as sr
-from gpt3 import GPT3
-from playsound import playsound
+import requests
 
-#设置API_KEY
-API_KEY = os.getenv('APIKEY')
 
-# 前台显示部分
-st.title('在线聊天机器人')
-st.write('请单击**[开始]**开始聊天。')
+@st.cache  # 语音识别 API 将会缓存
+def recognize_speech(wav_data):
+    url = '9exsX1dSHPQVpdb9Qqbscd4S'
+    wav_data = wav_data.read()
+    response = requests.post(url, data=wav_data).json()
+    return response
 
-# 按钮点击事件
-if st.button('开始'):
-    # 初始化语音识别对象
-    r = sr.Recognizer()
-    # 初始化GPT3对象
-    gpt3 = GPT3(key=API_KEY)
-    
-    # 对话开始
+
+# 定义按钮动作
+def record_action(btn):
+    # 显示录音按钮
+    if btn:
+        st.audio('''
+        data:audio/wav;base64,UklGRjQGAABXQVZFZm10IBAAAAABAAEARKwAAIhYAQACABAAZGF0YQQBAAD/////
+CAIFAEAcAHqGvgH/AAABAAgAJAUAJwC4Bihm1gH/AwACABEAFQBDACUA8tq3AH/AAA
+BAAEACgBXAG8A5QPSMsoA/wMAHwAAAF8ATQCHAMMAjScSAP8ABQAWABQAAQDjAAf/
+BwC4Bihm1gH/AwATEgAyAFsAUgHjAO8A+//uAP8AAQATACsAXQCKAMsAmEHM/6QA
+/wEAGgBXAFoAWQB3AKoD3P/RAf8AAgB6AGQAfQBOAJYAwSCI/6UAhP8DABYAHgAr
+AIEAlADIARIB8v+eA/8ABQA7AEIAZACFAMAApSCu/6sA/wQAHwCAAIEA7AC/ATIB
+m/+7Af8ABQA1AGQARgBlAG4A4f+GAP8DAE4AMABqAHoAewC0AONAHv/XAP8ABQAs
+AD0ARgBeAKMAiP+lAP8DAQAWACoAVQBzAP4AQ//gAP8AAgA4AEEAdwBOAMkAkf+g
+AP8DAE0AHAArAFcAlgDCALX/2QH/AAIAGAAgAHIALwD5AM8Ay/+UAGYAZgBob25n
+        ''', format='audio/wav', start_playing=True)
+        btn.label = '正在录音...'
+        response = recognize_speech(st.file_uploader("", type="audio/wav"))
+        st.write('你说：', response['text'])
+        btn.label = "开始录音"
+        return response['text']
+
+
+# 主函数
+def main():
+    btn = st.button("开始录音")
+    # 获取录音结果
+    st.write("请问有什么可以帮助您？")
+    user_input = record_action(btn)
+
+    # 将用户输入拼接到聊天内容末尾
+    msg = '你可以说：再见'
+    response = user_input
+
+    # 定义停止循环标志
+    goodbye = False
+    # 开启循环
     while True:
-        # 提示用户开始说话
-        st.write('开始说话，或按下按钮说话')
-        if st.button('说话'):
-            # 开始录音
-            with sr.Microphone() as source:
-                audio_data = r.listen(source)
-            try:
-                # 将语音转换为文字
-                text = r.recognize_google(audio_data)
-            except:
-                st.write('没有听到你说什么，请重试！')
-            else:
-                # 使用chatgpt-V3 api处理语音文字消息  
-                answer = gpt3.ask(text)
-                # 显示机器人回复
-                st.write(answer)
-                # 朗读机器人回复
-                playsound(answer)
-        
-        # 如果用户说再见，则结束循环
-        if text == '再见':
+        st.write(msg)
+        # 向 chatbot 发送用户输入，获取 chatbot 回答
+        response = requests.post('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', data={'text': response}).json()['response']
+        st.write(response)
+
+        # 检测用户最后输入是否为再见，如果是的话，停止循环
+        if response[-2:] == '再见':
+            goodbye = True
+            st.write('欢迎下次使用～')
+
+        # 如果用户没有说再见，重新启动循环
+        if not goodbye:
+            btn = st.button("开始录音")
+            # 获取录音结果
+            response = record_action(btn)
+        else:
             break
 
+
+if __name__ == '__main__':
+    main()
+
+```
