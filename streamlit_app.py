cef06cd8a1b096904e4fbfbe5d2da7854c543a0f
潘振兴 <53480775+Pan-zhenxing@users.noreply.github.com>
2023-03-03 20:17:16 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 import streamlit as st
-from PIL import Image
+from PIL import Image, ImageFilter
 import requests
 from io import BytesIO
+
+# 添加标题
+st.title("程序反馈微信：Allin6118")
+st.write("123")
 
 # 定义一个函数，用于下载处理后的图片
 def download_img(img):
