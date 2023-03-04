6d9d0be74f001c0c31cfe1c8789a47b9c6e6b5a9
潘振兴 <53480775+Pan-zhenxing@users.noreply.github.com>
2023-03-04 11:49:45 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,33 @@
 import streamlit as st
-import requests
 from PIL import Image
+import requests
 from io import BytesIO
 
-st.set_page_config(page_title="图片剪裁", layout="wide")
+# 设置应用标题和页面布局
+st.set_page_config(page_title="图片剪切工具", layout="wide")
 
-st.sidebar.title("选择图片")
-url = st.sidebar.text_input("输入图片URL")
-if url:
-    response = requests.get(url)
-    img = Image.open(BytesIO(response.content))
+# 定义一个侧边栏，用于输入图片地址或上传本地图片
+st.sidebar.title("上传或输入图片")
+input_type = st.sidebar.radio("选择图片类型", ["URL", "本地文件"])
+if input_type == "URL":
+    url = st.sidebar.text_input("输入图片URL")
+    if url:
+        response = requests.get(url)
+        img = Image.open(BytesIO(response.content))
+else:
+    uploaded_file = st.sidebar.file_uploader("上传图片", type=["png", "jpg", "jpeg"])
+    if uploaded_file is not None:
+        img = Image.open(uploaded_file)
+
+# 如果有图片输入，则显示原图
+if "img" in locals():
     st.image(img, caption="原图", width=500)
 
-    st.sidebar.title("选择剪裁比例")
-    ratio = st.sidebar.radio("比例", ["原比例", "16:9", "4:3"])
+    # 定义一个侧边栏，用于选择剪切比例
+    st.sidebar.title("剪切比例")
+    ratio = st.sidebar.radio("选择图片比例", ["16:9", "4:3", "自定义"])
 
+    # 如果用户选择了比例，则进行裁剪
     if ratio == "16:9":
         width, height = img.size
         if width * 9 > height * 16:
@@ -39,13 +52,20 @@ if url:
             top = (height - new_height) // 2
             bottom = top + new_height
             img = img.crop((0, top, width, bottom))
+    else:
+        # 自定义比例，需要用户输入剪切的区域
+        left = st.sidebar.number_input("左", 0, img.size[0], step=1)
+        top = st.sidebar.number_input("上", 0, img.size[1], step=1)
+        right = st.sidebar.number_input("右", 0, img.size[0], step=1)
+        bottom = st.sidebar.number_input("下", 0, img.size[1], step=1)
+        img = img.crop((left, top, right, bottom))
 
-    st.image(img, caption="剪裁后的图片", width=500)
-
-    st.sidebar.title("保存图片")
-    file_name = st.sidebar.text_input("输入保存文件名", value="cropped_image.jpg")
-    if st.sidebar.button("保存图片"):
-        img.save(file_name)
-
-else:
-    st.sidebar.text("请输入图片URL")
+    # 显示处理后的图片和下载按钮
+    st.image(img, caption="处理后的图片", width=500)
+    st.download_button(
+        label="下载图片",
+        data=BytesIO(img.convert("RGB").tobytes()),
+        file_name="processed_image.jpg",
+        mime="image/jpg",
+    )
+``
